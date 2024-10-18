4e260be76052428b87c28465d6b7ba3b874e326c
junji <sinojunji@gmail.com>
2024-10-18 22:08:36 +0800

    initial commit

---
+++ src/test/java/com/joyle/chatbot/ChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyle.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
